04b28099ac47ee2ee7fd2bd3f6edb3a7da339d75
Rodrigo Arana <103905536+Klsie@users.noreply.github.com>
2024-04-24 18:10:40 -0600

    Update Ggrafos.java

--- Ggrafos.java
+++ Ggrafos.java
@@ -18,12 +18,12 @@ public class Ggrafos {
         grafo.nuevoNodo(nodo5);
         
         
-        grafo.nuevoArco(nodo5, nodo3,4); // Arco entre nodo 5 y nodo 3
-        grafo.nuevoArco(nodo4, nodo2,53); // Arco entre nodo 4 y nodo 2
-        grafo.nuevoArco(nodo1, nodo4,12); // Arco entre nodo 1 y nodo 4
-        grafo.nuevoArco(nodo3, nodo1,12); // Arco entre nodo 3 y nodo 1
-        grafo.nuevoArco(nodo2, nodo1,8); // Arco entre nodo 2 y nodo 1
-        grafo.nuevoArco(nodo2, nodo5,10); // Arco entre nodo 2 y nodo 5
+        grafo.nuevoArco(nodo5, nodo3,4); 
+        grafo.nuevoArco(nodo4, nodo2,53); 
+        grafo.nuevoArco(nodo1, nodo4,12); 
+        grafo.nuevoArco(nodo3, nodo1,12);
+        grafo.nuevoArco(nodo2, nodo1,8); 
+        grafo.nuevoArco(nodo2, nodo5,10); 
 
         System.out.println(grafo.mostrar());
 
